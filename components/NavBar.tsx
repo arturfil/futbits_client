500db268f5550bc44c4c9fa3851d699f238272db
arturfil <arturfil@hotmail.com>
2023-03-02 21:10:49 -0500

    added mobile responsiveness to the sidemenu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,26 +1,22 @@
-import React, {  } from "react";
-import {
-  Box,
-  IconButton,
-} from "@mui/material";
+import React from "react";
+import { Box, IconButton } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 
 // icons
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import NotificationsOutlineIcon from '@mui/icons-material/NotificationsOutlined';
-import SettingsOutlineIcon from '@mui/icons-material/SettingsOutlined';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutlined';
+import NotificationsOutlineIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlineIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutlined";
 import { setLogIn } from "../features/account/accountSlice";
 
 interface Props {
-  display: boolean
-  setDisplay: Function
+  display: boolean;
+  setDisplay: Function;
 }
 
-export default function NavBar({display, setDisplay}: Props) {
+export default function NavBar({ display, setDisplay }: Props) {
   const dispatch = useAppDispatch();
   const { isLoggedIn } = useAppSelector((state) => state.account);
-  
 
   const logOut = () => {
     localStorage.removeItem("jwt_gochi");
@@ -28,20 +24,17 @@ export default function NavBar({display, setDisplay}: Props) {
   };
 
   return (
-    <Box 
-      className="menu"
-      sx={{display: "flex", 
-        justifyContent: "end",
-        padding: 2
-        }}
-    > 
-      <Box sx={{display:"flex", }}>
-        <IconButton>
-          {/* {themeColor === "dark" ? (
-            <LightModeOutlinedIcon/>
-            ): (
-            <DarkModeOutlinedIcon/>
-          )} */}
+    <Box
+      className="topbar"
+      sx={{ display: "flex", justifyContent: "start", padding: 2 }}
+    >
+      {/* <Box sx={{display:"flex", }}> */}
+      <MenuOutlinedIcon
+        sx={{ color: "#6d6d6d" }}
+        className="menu"
+        onClick={() => setDisplay(!display)}
+      />
+      {/* <IconButton>
         </IconButton>
         <IconButton>
           <NotificationsOutlineIcon/>
@@ -52,7 +45,7 @@ export default function NavBar({display, setDisplay}: Props) {
         <IconButton>
           <PersonOutlineIcon/>
         </IconButton>
-      </Box>
+      </Box> */}
     </Box>
   );
 }
